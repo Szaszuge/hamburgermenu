fd374ec89a723366618e635bcdb92cbce53a91d8
Szaszuge <78954822+Szaszuge@users.noreply.github.com>
2022-09-29 08:24:28 +0200

    kód elsajátítása(átírtam mindent magyarra)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+/*Kapcsolja be a linkek mutatását amikor a felhasználó rákattint a menüre vagy az ikonra*/
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
